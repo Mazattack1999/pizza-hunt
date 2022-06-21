7707d3fed491cb1e9aaa2efcadf0c47736e5a41d
micah_z <micah.a.zahn@gmail.com>
2022-06-21 17:43:51 -0500

    add render pizza cards

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
 
-// add prefix of `/pizzas` to routes created in the `pizza-routes.js`
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
